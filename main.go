04cf96c3a05b06296e038818ed0f81cb39507363
mdjhny <yfx8899@gmail.com>
2026-07-31 16:01:13 +0800

    feat: add more cases

--- main.go
+++ main.go
@@ -18,13 +18,19 @@ const (
 )
 
 var classifier = map[string]protocol{
-	"A single-node application":                            None,
-	"PostgreSQL primary with read replicas":                None,
-	"HashiCorp Consul KV store":                            Raft,
-	"Service mesh discovering 1000s of nodes":              Gossip,
-	"Distributed ledger across mutually-untrusted parties": Bft,
-	"Internal Google service replicating metadata":         Paxos,
-	"3-node etcd cluster":                                  Raft,
+	"A single-node application":                               None,
+	"PostgreSQL primary with read replicas":                   None,
+	"HashiCorp Consul KV store":                               Raft,
+	"Service mesh discovering 1000s of nodes":                 Gossip,
+	"Distributed ledger across mutually-untrusted parties":    Bft,
+	"Internal Google service replicating metadata":            Paxos,
+	"3-node etcd cluster":                                     Raft,
+	"Blockchain replication across competing miners":          Bft,
+	"Kubernetes control plane state store":                    Raft,
+	"Google Chubby lock service":                              Paxos,
+	"Cassandra-like ring membership of 1000 nodes via gossip": Gossip,
+	"CockroachDB ranges":                                      Raft,
+	"Single-node Redis instance":                              None,
 }
 
 func main() {
